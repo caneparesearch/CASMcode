60b00a028893857ecc4f5a1e8224aaadf841f88c
Zeyu Deng <dengzeu@gmail.com>
2019-11-25 05:13:59 +0800

    update

--- src/casm/monte_carlo/grand_canonical/ChargeNeutralGrandCanonical.cc
+++ src/casm/monte_carlo/grand_canonical/ChargeNeutralGrandCanonical.cc
@@ -1,6 +1,6 @@
 #include "casm/monte_carlo/grand_canonical/ChargeNeutralGrandCanonical.hh"
 #include "casm/clex/PrimClex.hh"
-#include "casm/monte_carlo/grand_canonical/GrandCanonicalIO.hh"
+#include "casm/monte_carlo/grand_canonical/ChargeNeutralGrandCanonicalIO.hh"
 #include "casm/monte_carlo/MonteIO_impl.hh"
 
 
