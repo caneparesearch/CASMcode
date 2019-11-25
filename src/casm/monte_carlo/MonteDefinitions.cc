a3bdb9f7e270a62c1cafd8abf065c965adca9671
Zeyu Deng <dengzeu@gmail.com>
2019-11-25 10:07:30 +0800

    update

--- src/casm/monte_carlo/MonteDefinitions.cc
+++ src/casm/monte_carlo/MonteDefinitions.cc
@@ -3,11 +3,10 @@
 namespace CASM {
 
   const std::string traits<Monte::ENSEMBLE>::name = "ensemble";
-// Zeyu: I added this
+  
   const std::multimap<Monte::ENSEMBLE, std::vector<std::string> > traits<Monte::ENSEMBLE>::strval = {
     {Monte::ENSEMBLE::GrandCanonical, {"GrandCanonical", "grand_canonical"} },
-    {Monte::ENSEMBLE::Canonical, {"Canonical", "canonical"} },
-    {Monte::ENSEMBLE::ChargeNeutralGrandCanonical, {"ChargeNeutralGrandCanonical", "charge_neutral_grand_canonical"} }
+    {Monte::ENSEMBLE::Canonical, {"Canonical", "canonical"} }
   };
 
 
