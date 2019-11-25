f2c07e9b01d1e8d740866b1dabd8bb66d2a8b178
Zeyu Deng <dengzeu@gmail.com>
2019-11-25 10:10:19 +0800

    update

--- src/casm/monte_carlo/MonteDefinitions.cc
+++ src/casm/monte_carlo/MonteDefinitions.cc
@@ -3,10 +3,11 @@
 namespace CASM {
 
   const std::string traits<Monte::ENSEMBLE>::name = "ensemble";
-  
+
   const std::multimap<Monte::ENSEMBLE, std::vector<std::string> > traits<Monte::ENSEMBLE>::strval = {
     {Monte::ENSEMBLE::GrandCanonical, {"GrandCanonical", "grand_canonical"} },
-    {Monte::ENSEMBLE::Canonical, {"Canonical", "canonical"} }
+    {Monte::ENSEMBLE::Canonical, {"Canonical", "canonical"} },
+    {Monte::ENSEMBLE::ChargeNeutralGrandCanonical, {"ChargeNeutralGrandCanonical", "charge_neutral_grand_canonical"} }
   };
 
 
