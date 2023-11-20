10e3f1875f677828877d54cdd833cfed4f2b175a
root <e0546262@u.nus.edu>
2023-11-20 09:39:43 +0000

    update for singularity

--- test_Fvib/T_up_0/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-rm -rf conditions.* stdout.txt results*
-
-casm monte -s monte.json > stdout.txt
--- test_Fvib/fit/add_weight.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-FILE1='casm_learn_input'
-while IFS= read -r line
-do
-    awk '$27= "1.0"' casm_learn_input > tmp.txt && mv tmp.txt file.txt
-done <"$FILE1"
