d50656996c64acb24d42401340abff02caa04f8e
Hengning-Chen <e0546262@u.nus.edu>
2024-08-14 20:00:30 +0800

    correction

--- src/casm/monte_carlo/grand_canonical/GrandCanonical.cc
+++ src/casm/monte_carlo/grand_canonical/GrandCanonical.cc
@@ -35,9 +35,9 @@ namespace CASM {
     GrandCanonical::set_T_initial_dT(T_initial,dT);
     // std::cerr << "Value of temperature1: " << desiredT << std::endl;
     // for docker at orion (test gcMC extract and interpolate Fvib correctly)
-    std::string filename = "/userhome1/hengning/Fvib_CASMcode/CASMcode/prim_beta.csv";
+    // std::string filename = "/userhome1/hengning/Fvib_CASMcode/CASMcode/prim_beta.csv";
     // for read-only singularity at fornax
-    // std::string filename = "/app/CASMcode/prim_beta.csv";
+    std::string filename = "/app/CASMcode/prim_beta.csv";
     // for read-only singularity at fornax to build Ta system
     // std::string filename = "/app/CASMcode/prim_alpha.csv";
     GrandCanonical::interpolate_vib_formation_energy(filename);
--- start.sh
+++ start.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+docker start test_fvib_casm_container
 docker exec -it -w $PWD test_fvib_casm_container bash
